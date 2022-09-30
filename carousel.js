6fbc7aa2831fbebe554fb7b8349a19ad9614350a
AndrewScott85 <andrewgscott@gmail.com>
2022-09-30 15:02:06 +0100

    added in Tubular slide

---
+++ carousel.js
@@ -0,0 +1,12 @@
+
+const glide = new Glide('.glide',{
+    type: 'carousel',
+    animationDuration: 1000,
+    focusAt: '1',
+    startAt: 1,
+    perView: 1,
+    peek: 20,
+    perTouch: 1,
+});
+
+glide.mount();
